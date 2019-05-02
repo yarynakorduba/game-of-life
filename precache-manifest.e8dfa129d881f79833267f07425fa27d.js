86b463d3e6d73fa99148a68a9ced3fd88cb3b676
yarynakorduba <korduba_y@ucu.edu.ua>
2019-05-03 02:37:38 +0300

    Updates

---
+++ precache-manifest.e8dfa129d881f79833267f07425fa27d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616329dd27cbf4ac3095c2de8a9b2d37",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "8e53f553e7b534c3aef4",
+    "url": "/game-of-life/static/css/main.e0aded7f.chunk.css"
+  },
+  {
+    "revision": "201ea843c489cea52226",
+    "url": "/game-of-life/static/js/2.732e3a7a.chunk.js"
+  },
+  {
+    "revision": "8e53f553e7b534c3aef4",
+    "url": "/game-of-life/static/js/main.33411046.chunk.js"
+  },
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  }
+]);
